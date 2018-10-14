a6a7b3d4415d11cf6e8fa824aeeb99c70f833dd5
Raphael Prosillo <raphael.prosillo@gmail.com>
2018-10-14 20:58:01 +0200

    .

---
+++ Elementary/06_best-stock.py
@@ -0,0 +1,31 @@
+def best_stock(data):
+    mexp = 0
+    res = ''
+    for value in data:
+        if data[value] > mexp:
+            mexp = data[value]
+            res = value
+        return res
+
+return 'GOOG'
+
+if __name__ == '__main__':
+    print("Example:")
+    print(best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert best_stock({
+        'CAC': 10.0,
+        'ATX': 390.2,
+        'WIG': 1.2
+    }) == 'ATX', "First"
+    assert best_stock({
+        'CAC': 91.1,
+        'ATX': 1.01,
+        'TASI': 120.9
+    }) == 'TASI', "Second"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
